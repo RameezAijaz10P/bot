733bdd13c30d6c21765f84db3f92fc56437e97b5
Shruthi <sxrajamohan@shockers.wichita.edu>
2016-11-13 15:18:41 -0600

    group the date with hours worked

--- back_end/timetrack/app/controllers/rest/v1/timesheet_controller.rb
+++ back_end/timetrack/app/controllers/rest/v1/timesheet_controller.rb
@@ -1,10 +1,12 @@
 module Rest
   module V1
     class TimesheetController < ApplicationController
+      #respond_to :json
+
       # creates new clock_in entry and updates the clock_out
       def clock
          action = params[:test]
-         time = Time.now.in_time_zone("Central Time (US & Canada)")
+         time = Time.now.tomorrow.in_time_zone("Central Time (US & Canada)")
          currenttime = time.strftime("%I:%M %P")
 
         if action == "clock_in"
@@ -12,32 +14,53 @@ module Rest
                                   user_id: params[:project_id],
                                   slack: params[:slack],date: time.to_date, start_time: currenttime)
           if @timesheet.save
-            render json: {status: "success", data: @timesheet}, status: :created
+            render json: {status: "success"}, status: :OK
           else
             render json: { errors: @timesheet.errors }, status: :unprocessable_entity
           end
 
-      elsif action == "clock_out"
-        @timesheet = Timesheet.where('user_id = ? AND project_id = ? AND end_time IS ?', params[:user_id], params[:project_id], nil)
-        if @timesheet.update(end_time: currenttime)
-          render json: {status: "success", data: @timesheet}, status: :ok
-        else
-          render json: { errors: @timesheet.errors }, status: :unprocessable_entity
-        end
-      end
-    end
-    # fetch the history for given date
-    def history
-      if params['start_date']!= nil && params['end_date'] != nil
-        @start_date = params['start_date'].to_date
-        @end_date = params['end_date'].to_date
-        @timesheet = Timesheet.where(date: @start_date..@end_date)
+        elsif action == "clock_out"
+              @timesheet = Timesheet.where('user_id = ? AND project_id = ? AND end_time IS ?', params[:user_id], params[:project_id], nil)
+              start = @timesheet.pluck(:start_time)
+              endtime = Time.parse(currenttime)
+              starttime = Time.parse(start[0].strftime("%I:%M %P"))
+              hours = ((endtime - starttime)/ 1.hour).round(2)
 
-    else
-      @timesheet = Timesheet.all
-    end
-        render json: {status: "success", data: @timesheet}
-    end
-  end
-end
-end
+              if @timesheet.update(end_time: currenttime,hours: hours)
+                render json: {status: "success"}, status: :ok
+              else
+                render json: { errors: @timesheet.errors }, status: :unprocessable_entity
+              end
+            end
+          end
+          # fetch the history for given date
+          def history
+            if params['start_date']!= nil && params['end_date'] != nil
+              @start_date = params['start_date'].to_date
+              @end_date = params['end_date'].to_date
+              @timesheet = Timesheet.where(date: @start_date..@end_date)
+
+          else
+            @timesheet = Timesheet.all
+          end
+              render json: {status: "success", data: @timesheet}
+          end
+
+          #fetch the hours for the dates in the table
+          def hours
+            @timesheet = Timesheet.select("hours,date").group('date').sum(:hours)
+            @newtimesheet = Hash.new
+            array = []
+            @timesheet.each_pair { |key, value|
+
+              @newtimesheet["date"]= key
+              @newtimesheet["hours"]= value
+              array.push(@newtimesheet)
+
+            }
+            puts array
+            render json: array
+            end
+          end
+      end
+      end
--- back_end/timetrack/db/migrate/20161112003339_create_timesheets.rb
+++ back_end/timetrack/db/migrate/20161112003339_create_timesheets.rb
@@ -5,6 +5,7 @@ class CreateTimesheets < ActiveRecord::Migration[5.0]
       t.time :start_time
       t.time :end_time
       t.integer :slack
+      t.float :hours
       t.integer :liquid_planner
       t.belongs_to :project, index: true
       t.belongs_to :user, index: true
