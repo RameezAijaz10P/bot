4f8eff9af0bfcd18fed4db7d6a4da179e354d621
rameez aijaz <sxaijaz@wichita.edu>
2016-11-12 18:34:01 -0600

    minor change

--- front_end/config/routes.js
+++ front_end/config/routes.js
@@ -96,26 +96,6 @@
                 }
 
             }
-        }).state('base_template.partials.project', {
-            url: "/project",
-            views:{
-                'content@base_template.partials':{
-                    templateUrl: "home/project.html",
-                    controller: "projectController",
-                    controllerAs: "projectController"
-                }
-
-            }
-        }).state('base_template.partials.projects', {
-            url: "/projects",
-            views:{
-                'content@base_template.partials':{
-                    templateUrl: "home/projects.html",
-                    controller: "projectsController",
-                    controllerAs: "projectsController"
-                }
-
-            }
         });
         /* Add New Routes Above */
         $urlRouterProvider.otherwise('/login');
